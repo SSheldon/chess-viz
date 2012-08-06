b615e17aadc14d33f41821dad75f579a8e116846
Steven Sheldon <steven@sasheldon.com>
2012-08-05 19:50:20 -0700

    Added Visualizer class using pyglet.

--- visualizer.py
+++ visualizer.py
@@ -1,5 +1,19 @@
 import re
 import chess
+import pyglet
+from pyglet.gl import *
+
+class Visualizer:
+	def __init__(self):
+		self.window = pyglet.window.Window()
+		self.window.set_handler('on_draw', self.onDraw)
+		glClearColor(1, 1, 1, 1)
+
+	def onDraw(self):
+		glClear(GL_COLOR_BUFFER_BIT)
+
+	def run(self):
+		pyglet.app.run()
 
 game = chess.Game()
 for row in game.show():
